78c900d4efaa5273a0ffe78ef4320c96f36dbc48
Uzair Shah <uzairshahh121@gmail.com>
2023-08-07 22:42:11 +0500

    search by name

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="flex flex-col p-8">
-    <div>
-      <input
-        type="text"
-        placeholder="Search for Meals"
-        class="rounded border-2 border-gray-200 w-full"
-      />
-    </div>
     <div class="flex justify-center gap-2 mt-2">
       <router-link
         :to="{ name: 'byLetter', params: { letter } }"
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>By Name</div>
+  <div class="p-8">
+    <input
+      type="text"
+      v-model="keyword"
+      placeholder="Search for Meals"
+      class="rounded border-2 border-gray-200 w-full"
+      @change="searchMeals"
+    />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const keyword = ref("");
+
+function searchMeals() {}
+</script>
