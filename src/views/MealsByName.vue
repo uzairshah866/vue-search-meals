8f08bb72615c276d210e9ba0c692421c72690409
Uzair Shah <uzairshahh121@gmail.com>
2023-08-13 21:55:25 +0500

    product card

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -8,8 +8,32 @@
       @change="searchMeals"
     />
   </div>
-  <div>
-    <pre>{{ meals }}</pre>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+    <div
+      v-for="meal of meals"
+      :key="meal.idMeal"
+      class="bg-white shadow rounded-xl"
+    >
+      <router-link to="/">
+        <img
+          :src="meal.strMealThumb"
+          :alt="meal.strMeal"
+          class="rounded-t-xl h-48 w-full object-cover"
+        />
+      </router-link>
+      <div class="p-3">
+        <h3 class="font-bold">{{ meal.strMeal }}</h3>
+        <p class="mb-4">{{ meal.strInstructions }}</p>
+        <div class="flex items-center justify-between">
+          <a
+            :href="meal.strYoutube"
+            target="_blank"
+            class="px-3 py-2 rounded border-2 border-red-600 bg-red-500 hover:bg-red-600 text-white transition-colors"
+            >YouTube</a
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
