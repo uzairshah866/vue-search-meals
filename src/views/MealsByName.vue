9a5522770577ba0618fbb44246e7e77d9acd951a
Uzair Shah <uzairshahh121@gmail.com>
2023-08-20 11:59:41 +0500

    meal item custom componnet

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -9,26 +9,7 @@
     />
   </div>
   <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-    <div
-      v-for="meal of meals"
-      :key="meal.idMeal"
-      class="bg-white shadow rounded-xl"
-    >
-      <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
-        <img
-          :src="meal.strMealThumb"
-          :alt="meal.strMeal"
-          class="rounded-t-xl h-48 w-full object-cover"
-        />
-      </router-link>
-      <div class="p-3">
-        <h3 class="font-bold">{{ meal.strMeal }}</h3>
-        <p class="mb-4">{{ meal.strInstructions }}</p>
-        <div class="flex items-center justify-between">
-          <YoutubeButton :href="meal.strYoutube">YouTube</YoutubeButton>
-        </div>
-      </div>
-    </div>
+    <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
   </div>
 </template>
 
@@ -36,7 +17,7 @@
 import store from "../store";
 import { useRoute } from "vue-router";
 import { ref, computed, onMounted } from "vue";
-import YoutubeButton from "../components/YoutubeButton.vue";
+import MealItem from "../components/MealItem.vue";
 
 const route = useRoute();
 const keyword = ref("");
